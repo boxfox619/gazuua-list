c5d616dd3d414f627874da9a1567a6de9ddc5a50
rlatjdfo112 <rlatjdfo112@naver.com>
2018-01-10 20:00:15 +0900

    [UPDATE] write the swagger json

---
+++ Server/app/docs/coin/coin.py
@@ -0,0 +1,26 @@
+COIN_GET = {
+    'tags': ['코인'],
+    'description': '코인들의 정보 조회',
+    'parameters': [],
+    'responses': {
+        '200': {
+            'description': '코인 목록 조회 성공',
+            'examples': {
+                'application/json': [
+                    {
+                        'rank': 1,
+                        'token': 'BTC',
+                        'name': 'BitCoin',
+                        'rate'": '12%'
+                    },
+                    {
+                        'rank': 2,
+                        'token': 'ETH',
+                        'name': 'Ethereum',
+                        'rate'": '-12%'
+                    }
+                ]
+            }
+        }
+    }
+}
---
+++ Server/app/docs/coin/recommend.py
@@ -0,0 +1,26 @@
+RECOMMEND_GET = {
+    'tags': ['추천코인'],
+    'description': '추천 코인 목록 조회',
+    'parameters': [],
+    'responses': {
+        '200': {
+            'description': '추천 코인 목록 조회 성공',
+            'examples': {
+                'application/json': [
+                    {
+                        'rank': 1,
+                        'token': 'BTC',
+                        'name': 'BitCoin',
+                        'rate'": '12%'
+                    },
+                    {
+                        'rank': 2,
+                        'token': 'ETH',
+                        'name': 'Ethereum',
+                        'rate'": '-12%'
+                    }
+                ]
+            }
+        }
+    }
+}

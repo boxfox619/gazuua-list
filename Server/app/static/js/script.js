695ca0192ab5c3244fa9355e2481d471ba40e0fe
rlatjdfo112 <rlatjdfo112@naver.com>
2018-01-12 00:59:05 +0900

    [Impl] loading coin information

--- Server/app/static/js/script.js
+++ Server/app/static/js/script.js
@@ -5,14 +5,30 @@ $( document ).ready(function() {
 
 function loadRecommends(){
   $.ajax({url: "/recommends", success: function(result){
-    console.log(result);
+    for (i = 0; i < result.length; i++) {
+      let coin = result[i];
+      $('#recommend-coins').append(createElement(coin));
+    }
+
   }});
 }
 
 function updateCoins(){
+  loadRecommends();
   $.ajax({url: "/coins", success: function(result){
-    console.log(result);
-
+    for (i = 0; i < result.length; i++) {
+      let coin = result[i];
+      $('#all-coins').append(createElement(coin));
+    }
     setTimeout(updateCoins, 3000)
   }});
+}
+
+function createElement(coin){
+  return ('<tr>'
+    + '<th></th>'
+    + '<th>'+coin['symbol']+'</th>'
+    + '<th>'+coin['name']+'</th>'
+    + '<th>'+coin['rate']+'</th>'
+    + '</tr>')
 }
